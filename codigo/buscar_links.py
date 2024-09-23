53fc2307877587d3d5cb419c2dfb512457d06410
João José Cardoso Ribeiro <joaojosecr@gmail.com>
2024-09-23 19:26:59 -0300

    Salvando músicas

--- codigo/buscar_links.py
+++ codigo/buscar_links.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import string
 import os
+from elasticsearch import Elasticsearch
 from datetime import datetime
 
 url = 'https://www.letras.com/letra/Z/artistas.html'
@@ -31,14 +32,32 @@ def get_musicas_de_artista(link_artista):
 
     soup = BeautifulSoup(html, 'html.parser')
     links_musicas = soup.find_all('a', {'class': 'songList-table-songName font --base --size16'})
-
+    artista = soup.find('h1', {'class': 'textStyle-primary'}).text[9:-5]
     lista_musicas = []
     for row in links_musicas:
         link_musica = row.attrs.get('href', '')
-        #link_artista = link_artista
         lista_musicas.append(link_musica)
     
-    return lista_musicas
+    return lista_musicas, artista
+
+def get_letra(link_musica):
+    link_musica = 'https://www.letras.com' + link_musica
+
+    html = get_html(link_musica)
+
+    soup = BeautifulSoup(html, 'html.parser')
+    title = soup.find('h1' , {'class': 'textStyle-primary'}).contents[0][9:-5]
+    lyrics = soup.find('div', {'class': 'lyric-original'})
+    lyrics = lyrics.find_all('p')
+    letra = ''
+    for line in lyrics:
+        letra = letra + ' ' + line.text
+        #letra.append(line.text)
+    
+    return letra, title
+
+
+
 
 def main():
 
@@ -47,24 +66,61 @@ def main():
 
     lista_artistas = []
 
-    #for letter in letras:
-    lista_artistas = get_artistas('Z')      # ALTERAR LETRA 'A' PARA LETTER DENTRO DO FOR 
-    caminho_nova_links = os.path.join(os.getcwd(), 'links')
-    # Verifica se a nova pasta já existe, caso contrário, cria a pasta
-    if not os.path.exists(caminho_nova_links):
-        os.makedirs(caminho_nova_links)
+    for letter in letras:
+        lista_artistas = get_artistas(letter)      
+        caminho_nova_links = os.path.join(os.getcwd(), 'links_')
+       
+       
+        if not os.path.exists(caminho_nova_links):
+            os.makedirs(caminho_nova_links)
 
-    for artista in lista_artistas:
-        lista_musicas = get_musicas_de_artista(artista)
-        if len(lista_musicas) > 0:
-            #caminho_arquivo = caminho_nova_pasta + '\\' + artista[1:-1] + '.txt'
-            with open(caminho_nova_links + '\\Z.txt', 'a') as arquivo:
-                for musica in lista_musicas:
-                    if musica[0] == '/':
-                        arquivo.write(musica+'\n')
+
+        for artista in lista_artistas:
+            lista_musicas = get_musicas_de_artista(artista)
+            if len(lista_musicas) > 0:
+                
+                with open(caminho_nova_links + '\\' + letter +'.txt', 'a') as arquivo:
+                    for musica in lista_musicas:
+                        if musica[0] == '/':
+                            arquivo.write(musica+'\n')
+
+
+def main_test():
+    
+    musicas = []
+   
+    lista_musicas, artista = get_musicas_de_artista('/mamonas-assassinas/')
+    
+    for link_music in lista_musicas:
+        letra,titulo = get_letra(link_music)
+        musica = {"_index": "songs", "_source": {
+            "title": titulo,
+            "artist": artista,
+            "lyrics": letra,
+            }}
+        musicas.append(musica)
+    
+    return musicas
+
 
 print(datetime.now())
 
-main()
+m = main_test()
 
 print(datetime.now())
+
+def set_musica_on_es(artista, titulo, letra):
+    es = Elasticsearch(["http://localhost:9200"])  # Substitua pela URL do seu Elasticsearch
+
+    # Dados da música que você quer enviar
+    musica = {
+        "title": titulo,
+        "artist": artista,
+        "lyrics": letra,
+    }
+
+    # Enviar a música para o índice 'songs'
+    response = es.index(index="songs", body=musica)
+
+    # Verificar a resposta
+    print(response) 
